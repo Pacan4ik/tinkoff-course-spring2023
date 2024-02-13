3f199644c411158529af0799ca1a3c682a4d5be9
Pacan4ik <andreyka.potekhin@mail.ru>
2024-02-13 23:31:09 +0300

    move message processors to edu.java.bot.processors folder

---
+++ bot/src/main/java/edu/java/bot/processors/UserMessageProcessor.java
@@ -0,0 +1,36 @@
+package edu.java.bot.processors;
+
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import edu.java.bot.commands.Command;
+import edu.java.bot.commands.CommandRegister;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMessageProcessor implements MessageProcessor {
+
+    private final CommandRegister commandRegister;
+
+    @Autowired UserMessageProcessor(CommandRegister commandRegister) {
+        this.commandRegister = commandRegister;
+    }
+
+    public List<? extends Command> commands() {
+        return commandRegister.commandList();
+    }
+
+    public SendMessage process(Update update) {
+        String text = update.message().text().split(" ")[0];
+        for (Command command : commandRegister.commandList()) {
+            if (text.equals(command.command())) {
+                return command.handle(update);
+            }
+        }
+        return new SendMessage(
+            update.message().chat().id(),
+            "Неизвестная команда. Введите /help для просмотра списка команд"
+        );
+    }
+}
