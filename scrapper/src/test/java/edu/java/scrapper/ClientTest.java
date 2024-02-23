e9982c7e54412171cf7a1a23457561aaa04f3dc6
Pacan4ik <andreyka.potekhin@mail.ru>
2024-02-23 20:59:06 +0300

    Move tests in single file

--- scrapper/src/test/java/edu/java/scrapper/ClientTest.java
+++ scrapper/src/test/java/edu/java/scrapper/ClientTest.java
@@ -9,7 +9,10 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Objects;
+import edu.java.clients.stackoverflow.StackOverFlowResponse;
+import edu.java.clients.stackoverflow.StackOverflowClient;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -19,8 +22,9 @@ import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class GitHubClientTest {
+public class ClientTest {
     static WireMockServer wireMockServer;
 
     @BeforeAll
@@ -36,14 +40,14 @@ public class GitHubClientTest {
     }
 
     @Test
-    void shouldGetResponse() throws IOException, URISyntaxException {
+    void shouldGetResponseGitHub() throws IOException, URISyntaxException {
         //given
         stubFor(
             get(urlEqualTo("/repos/testUser/testRep"))
                 .willReturn(aResponse()
                     .withHeader("Content-Type", "application/json")
                     .withBody(Files.readAllBytes(
-                            Path.of(Objects.requireNonNull(GitHubClientTest.class
+                            Path.of(Objects.requireNonNull(ClientTest.class
                                     .getResource("/GitHubResponseExample")
                                 ).toURI()
                             )
@@ -52,7 +56,6 @@ public class GitHubClientTest {
                 )
         );
         Client<GitHubResponse> client = new GitHubClient("http://localhost:8080");
-
 
         //when
         GitHubResponse gitHubResponse = client.fetchResponse("testUser", "testRep");
@@ -65,6 +68,44 @@ public class GitHubClientTest {
                 "https://github.com/Pacan4ik/tinkoff-course-spring2023",
                 null
             ), gitHubResponse
+
+        );
+
+    }
+
+    @Test
+    void shouldGetResponseStackOverflow() throws IOException, URISyntaxException {
+        //given
+        stubFor(
+            get(urlEqualTo(
+                "/2.3/questions/testUser"))
+                .willReturn(aResponse()
+                    .withHeader("Content-Type", "application/json")
+                    .withBody(Files.readAllBytes(
+                            Path.of(Objects.requireNonNull(ClientTest.class
+                                    .getResource("/StackOverflowResponseExample")
+                                ).toURI()
+                            )
+                        )
+                    )
+                )
+        );
+
+        Client<StackOverFlowResponse> client = new StackOverflowClient("http://localhost:8080");
+
+        //when
+        StackOverFlowResponse stackOverFlowResponse = client.fetchResponse("testUser");
+
+        //then
+        assertEquals(
+            new StackOverFlowResponse(
+                List.of(new StackOverFlowResponse.Item(
+                    OffsetDateTime.parse("2014-05-30T05:29:52Z"),
+                    OffsetDateTime.parse("2014-01-19T23:06:34Z"),
+                    "https://stackoverflow.com/questions/20089818/get-questions-content-from-stack-exchange-api",
+                    "Get questions content from Stack Exchange API"
+                ))
+            ), stackOverFlowResponse
 
         );
 
