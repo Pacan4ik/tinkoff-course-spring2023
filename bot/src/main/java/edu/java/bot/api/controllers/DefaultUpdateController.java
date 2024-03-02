985349ae4ba62bdb3023e93ba1aa7dd7e8ef9729
Pacan4ik <andreyka.potekhin@mail.ru>
2024-03-02 20:05:42 +0300

    bot api

---
+++ bot/src/main/java/edu/java/bot/api/controllers/DefaultUpdateController.java
@@ -0,0 +1,27 @@
+package edu.java.bot.api.controllers;
+
+import edu.java.bot.api.model.LinkUpdateRequest;
+import edu.java.bot.api.services.UpdateHandlerService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class DefaultUpdateController implements UpdateController {
+    UpdateHandlerService updateHandlerService;
+
+    public DefaultUpdateController(UpdateHandlerService updateHandlerService) {
+        this.updateHandlerService = updateHandlerService;
+    }
+
+    @Override
+    public ResponseEntity<Void> sendUpdates(LinkUpdateRequest linkUpdateRequest) {
+        updateHandlerService.handleUpdate(
+            linkUpdateRequest.url(),
+            linkUpdateRequest.description(),
+            linkUpdateRequest.tgChatIds()
+        );
+        return ResponseEntity.ok(null);
+    }
+}
