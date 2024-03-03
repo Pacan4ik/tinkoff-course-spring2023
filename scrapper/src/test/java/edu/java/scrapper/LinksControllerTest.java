de7726d88890bc322804eccf30eac359e207ff0d
Pacan4ik <andreyka.potekhin@mail.ru>
2024-03-03 18:20:25 +0300

    some tests

---
+++ scrapper/src/test/java/edu/java/scrapper/LinksControllerTest.java
@@ -0,0 +1,29 @@
+package edu.java.scrapper;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = {TestConfig.class})
+@Import(TestConfig.class)
+@AutoConfigureMockMvc
+public class LinksControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ChatRepository chatRepository;
+
+    @Test
+    @Disabled
+    void getLinks() throws Exception {
+        var ans = mockMvc.perform(get("/links").header("Tg-Chat-ID", 1L));
+        ans.andExpect(status().isOk());
+    }
+}
