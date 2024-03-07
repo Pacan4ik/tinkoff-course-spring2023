f5721ace4d9e353427215d829061cd3769693c2a
Pacan4ik <andreyka.potekhin@mail.ru>
2024-03-07 17:55:33 +0300

    Revert "done"
    This reverts commit 84592cb11e10a44d44224ae2e5e0777fb8bd6af2.

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,15 +1,5 @@
 package edu.java.scrapper;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.sql.Connection;
-import java.sql.SQLException;
-import liquibase.Liquibase;
-import liquibase.database.Database;
-import liquibase.database.DatabaseFactory;
-import liquibase.database.jvm.JdbcConnection;
-import liquibase.exception.LiquibaseException;
-import liquibase.resource.DirectoryResourceAccessor;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
@@ -21,7 +11,7 @@ public abstract class IntegrationTest {
     public static PostgreSQLContainer<?> POSTGRES;
 
     static {
-        POSTGRES = new PostgreSQLContainer<>("postgres:16")
+        POSTGRES = new PostgreSQLContainer<>("postgres:15")
             .withDatabaseName("scrapper")
             .withUsername("postgres")
             .withPassword("postgres");
@@ -31,33 +21,7 @@ public abstract class IntegrationTest {
     }
 
     private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        Connection connection;
-        try {
-            connection = c.createConnection("");
-            Database database =
-                DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
-            Liquibase liquibase = new Liquibase(
-                "master.xml",
-                new DirectoryResourceAccessor(
-                    new File(".")
-                        .toPath()
-                        .toAbsolutePath()
-                        .getParent()
-                        .getParent()
-                        .resolve("migrations")
-
-                )
-                , database
-            );
-
-
-            liquibase.update("");
-            //liquibase.update("", new OutputStreamWriter(System.out));
-
-        } catch (SQLException | LiquibaseException | FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
+        // ...
     }
 
     @DynamicPropertySource
