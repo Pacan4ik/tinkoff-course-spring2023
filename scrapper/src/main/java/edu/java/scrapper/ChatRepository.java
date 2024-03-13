f9573903544ebd99ac655c3233c309b76b02833a
Pacan4ik <andreyka.potekhin@mail.ru>
2024-03-14 00:09:45 +0300

    using a thread-safe implementation

--- scrapper/src/main/java/edu/java/scrapper/ChatRepository.java
+++ scrapper/src/main/java/edu/java/scrapper/ChatRepository.java
@@ -1,51 +1,48 @@
 package edu.java.scrapper;
 
 import java.net.URI;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import org.springframework.stereotype.Repository;
 
 @Repository
 //TODO DELETE
 public class ChatRepository {
-    Map<Long, List<URI>> map = new ConcurrentHashMap<>();
+    private final Map<Long, List<URI>> map = new ConcurrentHashMap<>();
 
     public boolean addUser(long id) {
-        if (map.containsKey(id)) {
-            return false;
-        }
-        map.put(id, new ArrayList<>());
+        map.putIfAbsent(id, new CopyOnWriteArrayList<>());
         return true;
     }
 
     public boolean deleteUser(long id) {
-        if (!map.containsKey(id)) {
-            return false;
-        }
         map.remove(id);
         return true;
     }
 
     public List<URI> getUserLinks(long id) {
-        return map.get(id);
+        return Collections.unmodifiableList(map.get(id));
     }
 
     public boolean addLink(long id, URI link) {
-        if (!map.containsKey(id)) {
-            return false;
-        }
-        var list = map.get(id);
-        return list.add(link);
+        map.computeIfPresent(id, (key, list) -> {
+                list.add(link);
+                return list;
+            }
+        );
+        return true;
     }
 
     public boolean deleteLink(long id, URI link) {
-        List<URI> list = map.get(id);
-        if (list == null) {
-            return false;
-        }
-        return list.remove(link);
+        map.computeIfPresent(id, (key, list) -> {
+                list.add(link);
+                return list;
+            }
+        );
+        return true;
     }
 
     public boolean checkUser(long id) {
