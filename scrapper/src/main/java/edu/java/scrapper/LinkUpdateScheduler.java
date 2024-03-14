317698d83ec39040bcaf210d2c8bccade165a3de
Pacan4ik <andreyka.potekhin@mail.ru>
2024-03-15 01:21:52 +0300

    remove unused import

--- scrapper/src/main/java/edu/java/scrapper/LinkUpdateScheduler.java
+++ scrapper/src/main/java/edu/java/scrapper/LinkUpdateScheduler.java
@@ -11,7 +11,6 @@ import edu.java.scrapper.configuration.ApplicationConfig;
 import java.net.URI;
 import java.time.OffsetDateTime;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
