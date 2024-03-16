3b228490fab380eddc42e0a1e7be66d042a54c15
Pacan4ik <andreyka.potekhin@mail.ru>
2024-03-16 20:40:40 +0300

    fix disabling scheduling

--- scrapper/src/main/java/edu/java/scrapper/scheduling/LinkUpdateScheduler.java
+++ scrapper/src/main/java/edu/java/scrapper/scheduling/LinkUpdateScheduler.java
@@ -8,11 +8,13 @@ import java.time.OffsetDateTime;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@ConditionalOnProperty(prefix = "app.scheduler", name = "enable")
 @RequiredArgsConstructor
 public class LinkUpdateScheduler {
     private final ApplicationConfig applicationConfig;
@@ -21,13 +23,15 @@ public class LinkUpdateScheduler {
 
     @Scheduled(fixedDelayString = "${app.scheduler.interval}")
     public void update() {
-        for (LinkDto linkDto : linkRepository.findAllWhereCheckedAtBefore(OffsetDateTime.now()
-            .minus(applicationConfig.scheduler().linkCheckingFrequency()))) {
-            try {
-                domainHandler.handle(linkDto);
-            } catch (Exception e) {
-                log.error(e.toString());
-                log.error(Arrays.toString(e.getStackTrace()));
+        if (applicationConfig.scheduler().enable()) {
+            for (LinkDto linkDto : linkRepository.findAllWhereCheckedAtBefore(OffsetDateTime.now()
+                .minus(applicationConfig.scheduler().linkCheckingFrequency()))) {
+                try {
+                    domainHandler.handle(linkDto);
+                } catch (Exception e) {
+                    log.error(e.toString());
+                    log.error(Arrays.toString(e.getStackTrace()));
+                }
             }
         }
     }
