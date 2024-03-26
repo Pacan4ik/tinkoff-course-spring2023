bab80339fefc6ea8672d61c28135cfb7039955ce
Andrey P <36331922+Pacan4ik@users.noreply.github.com>
2024-03-26 16:44:37 +0300

    Update bot/src/main/java/edu/java/bot/dataSources/SupportedLinkProvider.java
    Co-authored-by: Ilia Komarov <nexen505@yandex.ru>

--- bot/src/main/java/edu/java/bot/dataSources/SupportedLinkProvider.java
+++ bot/src/main/java/edu/java/bot/dataSources/SupportedLinkProvider.java
@@ -3,5 +3,5 @@ package edu.java.bot.dataSources;
 import edu.java.bot.utils.url.ParsedUrl;
 
 public interface SupportedLinkProvider {
-    boolean isSupports(ParsedUrl parsedUrl);
+    boolean isSupported(ParsedUrl parsedUrl);
 }
