ce802fa4bf2d5047528bb73902faa71c2e8bb5d8
Pacan4ik <andreyka.potekhin@mail.ru>
2024-04-05 15:13:24 +0300

    add spring.kafka.bootstrapservers in producer factory

--- scrapper/src/main/java/edu/java/scrapper/configuration/KafkaConfig.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/KafkaConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -15,12 +16,15 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @Configuration
 @RequiredArgsConstructor
 public class KafkaConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootStrapServers;
+
     private final ApplicationConfig applicationConfig;
 
     @Bean
     public ProducerFactory<String, BotUpdatesRequest> producerFactory() {
         Map<String, Object> configs = Map.of(
-            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers,
             ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
             ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class
         );
@@ -30,8 +34,7 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, BotUpdatesRequest>
     kafkaTemplate(ProducerFactory<String, BotUpdatesRequest> producerFactory) {
-        KafkaTemplate<String, BotUpdatesRequest> kafkaTemplate =
-            new KafkaTemplate<>(producerFactory);
+        KafkaTemplate<String, BotUpdatesRequest> kafkaTemplate = new KafkaTemplate<>(producerFactory);
         kafkaTemplate.setDefaultTopic(applicationConfig.kafka().producerTopic());
         return kafkaTemplate;
     }
