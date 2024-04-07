3394cee1d00ab6db3b838e4477f3cf375cc92bf0
Pacan4ik <andreyka.potekhin@mail.ru>
2024-04-07 17:32:32 +0300

    dlq redirect for failed messages

--- bot/src/main/java/edu/java/bot/configuration/KafkaConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/KafkaConfig.java
@@ -1,18 +1,23 @@
 package edu.java.bot.configuration;
 
-import edu.java.bot.api.model.LinkUpdateRequest;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
+@Slf4j
 public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootStrapServers;
@@ -21,31 +26,47 @@ public class KafkaConfig {
     private String groupId;
 
     @Bean
-    public ConsumerFactory<String, LinkUpdateRequest> consumerFactory() {
+    public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> configs = Map.of(
             ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers,
-            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class,
 
-            JsonDeserializer.VALUE_DEFAULT_TYPE, LinkUpdateRequest.class,
-            JsonDeserializer.USE_TYPE_INFO_HEADERS, false,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
 
             ConsumerConfig.GROUP_ID_CONFIG, groupId
         );
         return new DefaultKafkaConsumerFactory<>(
-            configs,
-            new StringDeserializer(),
-            new JsonDeserializer<>(LinkUpdateRequest.class)
+            configs
         );
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, LinkUpdateRequest>
-    containerFactory(ConsumerFactory<String, LinkUpdateRequest> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<String, LinkUpdateRequest> container =
+    public ConcurrentKafkaListenerContainerFactory<String, String>
+    containerFactory(
+        ConsumerFactory<String, String> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, String> container =
             new ConcurrentKafkaListenerContainerFactory<>();
         container.setConcurrency(1);
         container.setConsumerFactory(consumerFactory);
         return container;
     }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configs = Map.of(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers,
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
+        );
+        return new DefaultKafkaProducerFactory<>(configs);
+
+    }
+
+    @Bean
+    public KafkaTemplate<String, String>
+    kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
 }
--- bot/src/main/java/edu/java/bot/kafka/UpdatesListener.java
+++ bot/src/main/java/edu/java/bot/kafka/UpdatesListener.java
@@ -1,28 +1,57 @@
 package edu.java.bot.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.java.bot.api.model.LinkUpdateRequest;
 import edu.java.bot.api.services.UpdateHandlerService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-@RequiredArgsConstructor
 public class UpdatesListener {
     private final UpdateHandlerService updateHandlerService;
+    private final ObjectMapper objectMapper;
 
+    public UpdatesListener(UpdateHandlerService updateHandlerService) {
+        this.updateHandlerService = updateHandlerService;
+        this.objectMapper = new ObjectMapper();
+        this.objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        this.objectMapper.configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, true);
+    }
+
+    @RetryableTopic(
+        attempts = "1",
+        autoCreateTopics = "false",
+        kafkaTemplate = "kafkaTemplate",
+        dltTopicSuffix = "_dlq"
+    )
     @KafkaListener(topics = "${app.kafka.consumer-topic}",
                    containerFactory = "containerFactory")
-    public void listen(@Payload LinkUpdateRequest linkUpdateRequest) {
-        log.info("message has been received {}", linkUpdateRequest.toString());
-
+    public void listen(@Payload String data) {
+        log.info("message has been received {}", data);
+        LinkUpdateRequest linkUpdateRequest;
+        try {
+            linkUpdateRequest = objectMapper.readValue(data, LinkUpdateRequest.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         updateHandlerService.handleUpdate(
             linkUpdateRequest.url(),
             linkUpdateRequest.description(),
             linkUpdateRequest.tgChatIds()
         );
+    }
+
+    @DltHandler
+    public void handleDead(String payload, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        log.warn("Redirected to dlq from {}: {}", topic, payload);
     }
 }
