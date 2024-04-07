86bcdc31c303bc33f162b14e24ed844b2cb0188a
Pacan4ik <andreyka.potekhin@mail.ru>
2024-04-07 19:57:57 +0300

    test

--- bot/src/main/java/edu/java/bot/configuration/KafkaConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/KafkaConfig.java
@@ -9,6 +9,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -18,6 +19,7 @@ import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 @Slf4j
+@EnableKafka
 public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootStrapServers;
